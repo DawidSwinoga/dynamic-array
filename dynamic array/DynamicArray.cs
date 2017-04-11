447c6b2480e54ab7eb9dc1b73fb1d3d2bf3fe93c
Dawid Świnoga <dawid.swinoga@gmail.com>
2017-04-11 21:34:55 +0200

    Add project files.

---
+++ dynamic array/DynamicArray.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dynamic_array
+{
+    class DynamicArray
+    {
+        private static readonly log4net.ILog Log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private const int FirstIndexInArray = 0;
+        private const int DefaultArraySize = 2;
+        private int[] _array;
+        private int _maxCurrentSize;
+        private int _lastFilledIndex;
+
+        public DynamicArray() : this(size: DefaultArraySize)
+        {
+        }
+
+        public DynamicArray(int size)
+        {
+            _maxCurrentSize = size;
+            _lastFilledIndex = -1;
+            _array = new int[_maxCurrentSize];
+            Log.Debug("max current size = " + _maxCurrentSize);
+            Log.Debug("last fillded index = " + _lastFilledIndex + "\n");
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                Log.Debug("get index = " + index);
+                if (IsIndexInRange(index))
+                {
+                    return _array[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+
+            set
+            {
+                ResizeIfNecessary(index);
+                _array[index] = value;
+                _lastFilledIndex = index;
+                Log.Debug("set index = " + index + " value = " + value);
+                Log.Debug("last filled index = " + _lastFilledIndex);
+            }
+        }
+
+        public void Add(int item)
+        {
+            ResizeIfNecessary(++_lastFilledIndex);
+            _array[_lastFilledIndex] = item;
+            Log.Debug("add element = " + item);
+            Log.Debug("last filled index = " + _lastFilledIndex);
+        }
+
+        private bool IsIndexInRange(int index)
+        {
+            return index <= _lastFilledIndex && index >= FirstIndexInArray;
+        }
+
+        private void ResizeIfNecessary(int index)
+        {
+            if (index >= _maxCurrentSize)
+            {
+                _maxCurrentSize = index * 2;
+                Array.Resize(ref _array, _maxCurrentSize);
+                Log.Debug("max current size = " + _maxCurrentSize);
+            }
+        }
+    }
+}
---
+++ dynamic array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dynamic_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray dynamicArray = new DynamicArray();
+            for (int i = 0; i < 39; i++)
+            {
+                dynamicArray.Add(5);
+            }
+
+            try
+            {
+                int a = dynamicArray[200];
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            dynamicArray[100] = 10;
+            Console.WriteLine(dynamicArray[99]);
+
+            Console.ReadKey();
+        }
+    }
+}
